db62d3badaae0896ddeeb9615b8f5c2152fbc46c
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-01-18 10:39:13 +0000

    We added player names and testested

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -4,8 +4,17 @@ require("minitest/rg")
 require_relative("../Player")
 
 
-class TestBear < Minitest::Test
+class TestPlayer < Minitest::Test
   def setup
-    @bear = Bear.new("Big Tam")
-    @stomach = Array.new
+    @player1 = Player.new("Player1")
+    @player2 = Player.new("Player2")
+
   end
+
+def test_check_player_name
+  assert_equal("Player1" , @player1.player_name)
+ assert_equal("Player2" , @player2.player_name)
+ end
+end
+
+
